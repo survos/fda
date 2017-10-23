db923ac0bb7653b5fb52ac99c271474598d58522
karser <dpoddubny@gmail.com>
2017-10-23 12:43:34 +0300

    code container

--- docker/web/init_prod.sh
+++ docker/web/init_prod.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env sh
 
-target=/var/www
-if test "$(ls -A $target)"; then
-    echo "WARNING: directory $target is not empty"
-else
-    cp -R /source/* $target
-fi
-
 /wait-for-mysql.sh
 [[ $? -ne 0 ]] && exit $? # Exit if non-zero exit code
 
--- docker/web/init_test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-composer install
-
-/wait-for-mysql.sh
-[[ $? -ne 0 ]] && exit $? # Exit if non-zero exit code
-
-bin/load-data
-php bin/console server:run 0.0.0.0:${PORT}
